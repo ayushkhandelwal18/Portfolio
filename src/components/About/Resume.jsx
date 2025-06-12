457ecb970572d3cb5495ba6fc7e1c8451a1addbc
ayushkhandelwal18 <khandelwalayush976@gmail.com>
2025-06-12 23:33:04 +0530

    updated resume upload

--- src/components/About/Resume.jsx
+++ src/components/About/Resume.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { Container, Row, Button } from "react-bootstrap";
 import { AiOutlineDownload } from "react-icons/ai";
-import pdf from "../../Assets/Ayush Khandelwal.pdf"; 
 
 function Resume() {
   return (
     <Container>
-
       <p style={{ textAlign: "center", color: "#aaa", fontSize: "16px", marginBottom: "20px" }}>
-   Click below to view or download my resume and explore my professional journey.
-</p>
-    
+        Click below to view or download my resume and explore my professional journey.
+      </p>
 
-      {/* Download Button */}
       <Row style={{ justifyContent: "center", position: "relative", marginBottom: "20px" }}>
-        <Button variant="primary" href={pdf} target="_blank" style={{ maxWidth: "190px" }}>
+        <Button
+          variant="primary"
+          href="https://drive.google.com/file/d/1OzP-k9Ar2EzehjzaNqBpKRfSj4kZCVGP/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ maxWidth: "220px" }}
+        >
           <AiOutlineDownload />
-          &nbsp;Download Resume
+          &nbsp;View Resume
         </Button>
       </Row>
     </Container>
